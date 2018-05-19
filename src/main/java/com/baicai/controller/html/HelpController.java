0a2f93d8c90c0e755c3ffd6fa6fa376bbf0228c7
iminto <waitfox@qq.com>
2018-05-19 15:26:12 +0800

    实现了日志读取功能（不完善，目前还没实现倒序读取功能）

--- src/main/java/com/baicai/controller/html/HelpController.java
+++ src/main/java/com/baicai/controller/html/HelpController.java
@@ -1,8 +1,26 @@
 package com.baicai.controller.html;
 
 import com.baicai.core.ZKPlugin;
+import com.baicai.core.ZooLog;
 import com.github.zkclient.ZkClient;
 import com.jfinal.core.Controller;
+import org.apache.jute.BinaryInputArchive;
+import org.apache.jute.Record;
+import org.apache.zookeeper.server.TraceFormatter;
+import org.apache.zookeeper.server.persistence.FileHeader;
+import org.apache.zookeeper.server.persistence.FileTxnLog;
+import org.apache.zookeeper.server.util.SerializeUtils;
+import org.apache.zookeeper.txn.TxnHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.zip.Adler32;
+import java.util.zip.Checksum;
 
 /**
  * @author 猪肉有毒 waitfox@qq.com
@@ -12,15 +30,28 @@ import com.jfinal.core.Controller;
  */
 public class HelpController extends Controller {
 
+    public static Logger log = (Logger) LoggerFactory.getLogger(HelpController.class);
+
     public void index() {
         render("/admin-help.htm");
     }
 
     public void log() {
+        String logString="";
         ZkClient zkClient = getSessionAttr("zk-client");
         ZKPlugin zkPlugin = new ZKPlugin(zkClient);
         String[] configList = zkPlugin.fetchServerConfig();
         setAttr("config",configList);
+        try {
+            ZooLog zooLog = new ZooLog();
+            String dataDir = zooLog.getLogDir(configList);
+            String logFile = zooLog.read(dataDir);
+            zooLog.setLogFile(logFile);
+            logString = zooLog.getLastLog(100);
+        }catch (Exception e){
+            log.error("读取系统日志出错",e);
+        }
+        setAttr("logString", logString);
         render("/admin-log.htm");
     }
 }

cde8ccf998b005a29790e99315692d232b81b61f
iminto <waitfox@qq.com>
2018-05-19 22:29:35 +0800

    引入内嵌undertow，可以脱离容器运行

---
+++ src/main/java/com/baicai/core/Main.java
@@ -0,0 +1,40 @@
+package com.baicai.core;
+
+import com.jfinal.core.JFinalFilter;
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.server.handlers.PathHandler;
+import io.undertow.server.handlers.resource.FileResourceManager;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.DeploymentManager;
+import io.undertow.servlet.api.FilterInfo;
+import javax.servlet.DispatcherType;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        DeploymentInfo servletBuilder = Servlets.deployment()
+                .setContextPath("/")
+                .setClassLoader(Main.class.getClassLoader())
+                .setDeploymentName("zooadmin.war")
+                ;
+        FilterInfo jfinalFilter=new FilterInfo("jfinal",JFinalFilter.class);
+        jfinalFilter.addInitParam("configClass","com.baicai.core.Config");
+        servletBuilder.addFilter(jfinalFilter);
+        servletBuilder.addFilterUrlMapping("jfinal","/*", DispatcherType.REQUEST);
+        servletBuilder.addFilterUrlMapping("jfinal","/*", DispatcherType.FORWARD);
+        servletBuilder.setResourceManager(new FileResourceManager(new File("src/main/webapp"), 1024));
+
+        DeploymentManager manager = Servlets.defaultContainer().addDeployment(servletBuilder);
+        manager.deploy();
+        PathHandler path = Handlers.path(Handlers.redirect("/"))
+               .addPrefixPath("/", manager.start());
+        Undertow server = Undertow.builder()
+                .addHttpListener(1080, "localhost")
+                .setHandler(path)
+                .build();
+        // start server
+        server.start();
+    }
+}
